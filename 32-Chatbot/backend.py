7fa67101f784931af6faffbe03494eed6539ce25
keshav kandoi <kandoikeshav2004@gmail.com>
2026-04-04 14:25:53 +0530

    Streaming

--- 32-Chatbot/backend.py
+++ 32-Chatbot/backend.py
@@ -1,6 +1,6 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
@@ -30,3 +30,16 @@ graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+# using streaming 
+
+#  for adding  stream 
+
+
+# for message_chuck,meatadata in chatbot.stream( 
+#     {'message':[HumanMessage(content='what is the recipe to make pasta')]},
+#     config={"configurable":{'thread_id':'thread-1'}},
+#     stream_mode='messages'
+# ):
+#     if message_chuck.content:
+#         print(message_chuck.content,end=" ",flush=True)
