431f3ad09f4a37032f6b216c075c6fa0d426adb4
keshav kandoi <kandoikeshav2004@gmail.com>
2026-04-20 11:55:43 +0530

    using prompt

--- 37-GenAi-Practice/DAY1.py
+++ 37-GenAi-Practice/DAY1.py
@@ -2,11 +2,12 @@ import ollama
 from ollama import ChatResponse
 from ollama import chat
 
+prompt=input("Enter your text ")
 
 response=ollama.chat(model="qwen:7b",messages=[
   {
   "role":"user",
-  "content":"why sky is blue",
+  "content":prompt,
   }
 ],
  stream=True
