046939f1b822622968c2e1997584efb8f7471f0d
keshav kandoi <kandoikeshav2004@gmail.com>
2026-04-20 10:11:32 +0530

    streaming

--- 37-GenAi-Practice/DAY1.py
+++ 37-GenAi-Practice/DAY1.py
@@ -1,10 +1,16 @@
 import ollama
 from ollama import ChatResponse
+from ollama import chat
+
 
 response=ollama.chat(model="qwen:7b",messages=[
   {
   "role":"user",
-  "content":"why sky is blue"
-  },
-])
-print(response.message.content)
+  "content":"why sky is blue",
+  }
+],
+ stream=True
+ )
+for chunk in response:
+ print(chunk["message"]["content"],end="",flush=True)
+
